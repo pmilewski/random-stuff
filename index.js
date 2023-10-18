226abeff4c10f9e0621e29351c244362c20c9f3a
Piotr Milewski <milewski.piotr@gmail.com>
2023-10-18 20:16:00 +0200

    Add Apollo Server

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hi from index.js');
+const { ApolloServer, gql } = require('apollo-server');
+
+const PORT = process.env.PORT || 4000;
+
+const typeDefs = gql`
+  type Query {
+    greeting: String!,
+    interestingUrls: [String!]!,
+  }
+`;
+
+const data = {
+    greeting: 'Hello World!',
+    interestingUrls: [
+        'https://kodemia.mx',
+        'https://google.com',
+    ],
+};
+
+const server = new ApolloServer({ typeDefs, rootValue: data });
+
+server.listen().then(({ url }) => console.log(`Server started at ${url}`));
